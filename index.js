c9f805b5963d23289e0b1532e67e6c5862bd925a
BronwenM <bronwen.marchant@gmail.com>
2024-09-28 12:02:35 -0700

    OOPed it

--- index.js
+++ index.js
@@ -1,29 +1,77 @@
-let balance = 500.00;
-
-class Withdrawal {
-
-  constructor(amount) {
-    this.amount = amount;
+// let balance = 500.00;
+class Account {
+  constructor(username) {
+    this.username = username;
+    this.trasactions = [];
+  }
+  get balance() {
+    let balance = 0;
+    for (const transaction of this.trasactions) {
+      balance += transaction.value;
+    }
+    return balance;
   }
 
-  commit() {
-    balance -= this.amount;
+  addTransaction(transaction) {
+    this.trasactions.push(transaction);
   }
-
 }
 
+class Transaction {
+  constructor(amount, account) {
+    this.amount = amount;
+    this.account = account;
+  }
+  commit() {
+    if(!this.isAllowed()) return false;//transaction fails
+    this.time = new Date();
+    this.account.addTransaction(this);
+    return true;
+  }
+}
+
+class Deposit extends Transaction {
+  get value() {
+    return this.amount;
+  }
+
+  isAllowed(){
+    return true;
+  }
+}
+
+class Withdrawal extends Transaction {
+  get value() {
+    return -this.amount;
+  }
+
+  isAllowed(){
+    if(this.account.balance >= this.amount) return true;
+  }
+}
 
 
 
 // DRIVER CODE BELOW
 // We use the code below to "drive" the application logic above and make sure it's working as expected
+const myAccount = new Account("snow-patrol");
 
-t1 = new Withdrawal(50.25);
+t0 = new Deposit(120.0, myAccount);
+t0.commit();
+console.log('Transaction allowed?', t0.commit());
+console.log('balance:', myAccount.balance);
+
+t1 = new Withdrawal(50.25, myAccount);
 t1.commit();
-console.log('Transaction 1:', t1);
+console.log('Transaction allowed?', t1.commit());
+console.log('balance:', myAccount.balance);
 
-t2 = new Withdrawal(9.99);
+t2 = new Withdrawal(9.99, myAccount);
 t2.commit();
-console.log('Transaction 2:', t2);
+console.log('Transaction allowed?', t2.commit());
+console.log('balance:', myAccount.balance);
 
-console.log('Balance:', balance);
+t3 = new Withdrawal(120.0, myAccount);
+t3.commit();
+console.log('Transaction allowed?', t3.commit());
+console.log('balance:', myAccount.balance);
